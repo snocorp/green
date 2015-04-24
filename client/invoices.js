f5f74980d8b470ecc23c9befde77e4b202f8fb77
Dave Sewell <snocorp@gmail.com>
2015-04-23 23:08:59 -0400

    New Invoice Validation

--- client/invoices.js
+++ client/invoices.js
@@ -19,18 +19,53 @@ Template.Invoices.helpers({
   }
 });
 
+Template.InvoicesNew.onCreated(function() {
+  this.startError = new ReactiveVar('');
+  this.endError = new ReactiveVar('');
+});
+
 Template.InvoicesNew.events({
-  'change #new_invoice_period_start': function(event, template) {
-    Session.set('invoices_new_start', $('#new_invoice_period_start').val());
+  'change #invoices_new_start': function(event, template) {
+    Session.set('invoices_new_start', $('#invoices_new_start').val());
   },
-  'change #new_invoice_period_end': function(event, template) {
-    Session.set('invoices_new_end', $('#new_invoice_period_end').val());
+  'change #invoices_new_end': function(event, template) {
+    Session.set('invoices_new_end', $('#invoices_new_end').val());
   },
   'click #invoices_new_create_invoice': function(event, template) {
+    var error = false;
+    var start = template.find('#invoices_new_start').value;
+    var end = template.find('#invoices_new_end').value;
+
+    if (start === null || start.length === 0) {
+      template.startError.set('Start date is required');
+      error = true;
+    } else if (!moment(start, 'YYYY-MM-DD').isValid()) {
+      template.startError.set('Start date is not a valid date');
+      error = true;
+    } else {
+      template.startError.set('');
+    }
+
+    console.log(moment(start, 'YYYY-MM-DD'));
+
+    if (end === null || end.length === 0) {
+      template.endError.set('End date is required');
+      error = true;
+    } else if (!moment(end, 'YYYY-MM-DD').isValid()) {
+      template.endError.set('End date is not a valid date');
+      error = true;
+    } else {
+      template.endError.set('');
+    }
+
+    console.log(moment(end, 'YYYY-MM-DD'));
+
+    if (error) return;
+
     var id = Invoices.insert({
       userId: Meteor.userId(),
-      start: template.find('#new_invoice_period_start').value,
-      end: template.find('#new_invoice_period_end').value
+      start: start,
+      end: end
     });
 
     Router.go('invoices.show', {_id: id});
@@ -41,6 +76,12 @@ Template.InvoicesNew.events({
 });
 
 Template.InvoicesNew.helpers({
+  startError: function() {
+    return Template.instance().startError.get();
+  },
+  endError: function() {
+    return Template.instance().endError.get();
+  },
   start: function () {
     return Session.get('invoices_new_start');
   },
@@ -50,7 +91,7 @@ Template.InvoicesNew.helpers({
 });
 
 Template.InvoicesNew.onRendered(function() {
-  $('#new_invoice_daterange').datepicker({
+  $('#invoices_new_daterange').datepicker({
     format: "yyyy-mm-dd",
     orientation: "top auto"
   });
@@ -66,7 +107,7 @@ Template.InvoicesShow.helpers({
 Template.invoiceItems.events({
   'click #invoice_items_new_invoice_item': function() {
     var invoiceId = Iron.controller().params._id;
-      InvoiceItems.insert({
+    InvoiceItems.insert({
       invoiceId: invoiceId,
       itemId: null,
       quantity: 0
@@ -88,10 +129,10 @@ Template.invoiceItem.onCreated(function() {
 });
 
 Template.invoiceItem.events({
-  'click #invoice_item_edit_invoice_item': function(event, template) {
+  'click .invoice_item_edit_invoice_item': function(event, template) {
     template.editing.set(true);
   },
-  'click #invoice_item_edit_done': function(event, template) {
+  'click .invoice_item_edit_done': function(event, template) {
     var error = false;
     var id = Template.currentData()._id;
     var item = template.find('#invoice_item_item_' + id).value;
